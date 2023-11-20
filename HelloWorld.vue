7a9e9af21ee3487a5245a9209a8ec8cd12eef23d
Oscar <98938499+TuDeveloperWeb@users.noreply.github.com>
2023-11-19 23:07:53 -0500

    Add files via upload

---
+++ HelloWorld.vue
@@ -0,0 +1,140 @@
+<!-- App.vue -->
+<template>
+  <div id="app">
+    <div class="container">
+        <div class="target-area">
+          <!-- Contenido de tu área específica -->
+            <span> Hola Maria x ti voy a crear un Onboarding </span>
+        </div>
+  
+        <div>
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra nunc sapien, non rhoncus elit tincidunt vitae. Vestibulum maximus, ligula eu feugiat molestie, massa diam imperdiet odio, vitae viverra ligula est id nisi. Aliquam ut molestie est. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus at consequat dui.
+             Aenean tristique sagittis quam, sit amet sollicitudin neque sodales in.</p>
+
+          <div class="d-flex justify-content-between">
+              <b-card
+                title="Card Title"
+                img-src="https://picsum.photos/600/300/?image=25"
+                img-alt="Image"
+                img-top
+                tag="article"
+                style="max-width: 20rem;"
+                class="mb-2 "
+              >
+                <b-card-text>
+                  Some quick example text to build on the card title and make up the bulk of the card's content.
+                </b-card-text>
+            
+                <b-button href="#" variant="primary">Go somewhere</b-button>
+              </b-card>
+            
+              <b-card
+                title="Card Title"
+                img-src="https://picsum.photos/600/300/?image=25"
+                img-alt="Image"
+                img-top
+                tag="article"
+                style="max-width: 20rem;"
+                class="mb-2 ml-2"
+              >
+                <b-card-text>
+                  Some quick example text to build on the card title and make up the bulk of the card's content.
+                </b-card-text>
+            
+                <b-button href="#" variant="primary">Go somewhere</b-button>
+              </b-card>
+            
+              <b-card
+                title="Card Title"
+                img-src="https://picsum.photos/600/300/?image=25"
+                img-alt="Image"
+                img-top
+                tag="article"
+                style="max-width: 20rem;"
+                class="mb-2 "
+              >
+                <b-card-text>
+                  Some quick example text to build on the card title and make up the bulk of the card's content.
+                </b-card-text>
+            
+                <b-button href="#" variant="primary">Go somewhere</b-button>
+              </b-card>
+          </div>
+
+
+        </div>
+    </div>
+   
+    <Onboarding
+      v-if="showOnboarding"
+      @closeOnboarding="closeOnboarding"
+      :targetElement="targetElement"
+    />
+  </div>
+</template>
+
+<script>
+import Onboarding from '@/components/Onboarding.vue';
+
+export default {
+  data() {
+    return {
+      showOnboarding: false,
+      targetElement: null,
+    };
+  },
+
+  mounted() {
+    
+    this.obtenerClass();
+  },
+
+  methods: {
+
+    obtenerClass(){
+        const targetElement = document.querySelector('.target-area');
+        this.targetElement = targetElement;
+        console.log(targetElement);
+
+        if (this.targetElement ) {
+            this.showOnboarding = true;
+        }
+
+    },
+    openOnboarding(targetElement) {
+      this.showOnboarding = true;
+      this.targetElement = targetElement;
+    },
+    closeOnboarding() {
+      this.showOnboarding = false;
+      this.targetElement = null;
+    },
+    handleClick(event) {
+      // Verificar si el clic ocurrió dentro del área específica
+      const targetElement = event.target.closest('.target-area');
+      console.log(targetElement);
+      if (targetElement) {
+        this.openOnboarding(targetElement);
+      }
+    },
+  },
+  components: {
+    Onboarding,
+  },
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 60px;
+}
+
+.target-area {
+  background-color: #e0e0e0;
+  padding: 20px;
+  margin: 20px;
+  cursor: pointer;
+}
+</style>
---
+++ Onboarding.vue
@@ -0,0 +1,135 @@
+<!-- Onboarding.vue -->
+<template>
+    <div v-if="targetElement" class="onboarding">
+        <div class="modal-onboarding">
+            <div class="modal-onboarding__img">
+                <img src="https://static.figma.com/uploads/4e36d3bbbf8633fef1b86238f66bb1bf54f54d2a"
+                    width="120px" alt="Imagen" />
+            </div>
+            <div class="modal-onboarding__content">
+                <div class="modal-onboarding__text">
+                    Invite others or share this file with a link to jam
+                </div>
+
+                <div class="modal-onboarding__bottomContent">
+                    <div class="">
+                        <div>3 of 3</div>
+                    </div>
+                    
+                    <button>
+                        Next
+                    </button>
+                </div>
+            </div>
+            <!-- 
+        <div class="actions">
+          <button @click="prevStep" :disabled="currentStep === 0">Anterior</button>
+          <button @click="nextStep" :disabled="currentStep === steps.length - 1">Siguiente</button>
+          <button @click="closeOnboarding">Cerrar</button>
+        </div> -->
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    props: {
+        targetElement: {
+            type: HTMLElement,
+            default: null,
+        },
+    },
+    data() {
+        return {
+            currentStep: 0,
+            steps: [
+                { text: 'Paso 1: Descripción del paso 1' },
+                { text: 'Paso 2: Descripción del paso 2' },
+                // Agrega más pasos según sea necesario
+            ],
+        };
+    },
+
+    created() {
+        console.log(this.targetElement);
+    },
+
+    computed: {
+        currentStepText() {
+            return this.steps[this.currentStep].text;
+        },
+    },
+    methods: {
+        nextStep() {
+            if (this.currentStep < this.steps.length - 1) {
+                this.currentStep++;
+            }
+        },
+        prevStep() {
+            if (this.currentStep > 0) {
+                this.currentStep--;
+            }
+        },
+        closeOnboarding() {
+            this.$emit('closeOnboarding');
+        },
+    },
+};
+</script>
+  
+<style scoped>
+.onboarding {
+    position: fixed;
+    left: 908px;
+    top: 75.5px;
+    width: 409px;
+    height: 129px;
+    background: rgba(196, 189, 189, 0.5);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 10px;
+    border:1px solid #44009d;
+    padding: 3px;
+}
+
+.modal-onboarding{
+    display: flex;  
+}
+
+.modal-onboarding__img{
+    background-color: #c7baff;
+    padding: 5px;
+    width: 129px;
+    height: 129px;
+    display: flex;
+    align-items: center;
+    /* border-top: 1px solid #44009d;    */
+    justify-content: center;
+}
+
+.modal-onboarding__content{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    background-color: rgb(44, 48, 50);
+    color: #fff;
+    padding: 8px;
+
+}
+
+.modal-onboarding__bottomContent{
+ 
+    display: flex;
+    justify-content: space-between;
+}
+
+
+
+
+
+
+
+/* Resto del código de estilos */
+</style>
+  
